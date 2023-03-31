7a71ace6e3c74312de010d0af5a7cdb5970a8bf5
Shindu <gue.shindu@gmail.com>
2023-03-31 10:25:08 +0700

    commit 3

--- belanjaku/lib/main.dart
+++ belanjaku/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,7 +37,7 @@ class HalamanUtama extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                debugPrint("Loged & verified");
+                devtools.log("Logged & verified");
               } else {
                 return const HalamanVerifikasi();
               }
@@ -44,11 +45,82 @@ class HalamanUtama extends StatelessWidget {
               return const HalamanLogin();
             }
 
-            return Text('User loged: ${user.email}');
+            return const HalamanNotes();
           default:
             return const CircularProgressIndicator();
         }
       },
     );
   }
+}
+
+enum MenuAction { logout }
+
+class HalamanNotes extends StatefulWidget {
+  const HalamanNotes({Key? key}) : super(key: key);
+
+  @override
+  State<HalamanNotes> createState() => _HalamanNotesState();
+}
+
+class _HalamanNotesState extends State<HalamanNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belanja Ku'),
+        actions: [
+          PopupMenuButton<MenuAction>(onSelected: (value) async {
+            devtools.log(value.toString());
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogout = await logOutAlert(context);
+                if (shouldLogout) {
+                  devtools.log('User logout');
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/login/',
+                    (_) => false,
+                  );
+                }
+                break;
+            }
+          }, itemBuilder: (context) {
+            return [
+              const PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text("Logout"),
+              ),
+            ];
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+Future<bool> logOutAlert(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Yakin akan melakukan sign out?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Batal'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Ya'),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
